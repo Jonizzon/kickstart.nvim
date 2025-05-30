c23e50387ca710b233fe9fac3a754fb15c6892aa
Johan Nilsson <johan.nizzon@gmail.com>
2025-05-30 15:16:07 +0200

    Adds catppuccin and some other stuff

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -11,13 +11,13 @@ return {
   },
   lazy = false,
   keys = {
-    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+    { '§', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
   },
   opts = {
     filesystem = {
       window = {
         mappings = {
-          ['\\'] = 'close_window',
+          ['§'] = 'close_window',
         },
       },
     },
